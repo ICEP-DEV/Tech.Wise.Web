d90de30e74e00c967f005edacec84c76b0273b2b
Tonic-kiid <64985467+Tonic-kiid@users.noreply.github.com>
2024-09-18 09:32:52 +0200

    Add files via upload

---
+++ nthomemaps/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "nthomemapsc04fa00f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
